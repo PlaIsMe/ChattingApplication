6f512f09c06f1869398328735dc5a8719ea2bc32
PlaIsMe <phongvulai96@gmail.com>
2023-06-17 22:00:58 +0700

    [Phong] add springbootserver

---
+++ springbootserver/src/test/java/com/chattingapplication/springbootserver/SpringbootserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chattingapplication.springbootserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
