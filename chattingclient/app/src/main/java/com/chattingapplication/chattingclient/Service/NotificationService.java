e1d9c6b229c593ced17078c7494a1b36b30e4d9d
Hai Phan <phanthanhhai3317@gmail.com>
2023-07-02 20:34:05 +0700

    [Hai] - commit for pull

--- chattingclient/app/src/main/java/com/chattingapplication/chattingclient/Service/NotificationService.java
+++ chattingclient/app/src/main/java/com/chattingapplication/chattingclient/Service/NotificationService.java
@@ -2,13 +2,17 @@ package com.chattingapplication.chattingclient.Service;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
+import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
 
 import androidx.core.app.NotificationCompat;
 
+import com.chattingapplication.chattingclient.ChattingActivity;
 import com.chattingapplication.chattingclient.MainActivity;
 import com.chattingapplication.chattingclient.Model.Message;
 import com.chattingapplication.chattingclient.R;
@@ -18,6 +22,14 @@ import java.time.LocalDateTime;
 public class NotificationService {
 
     public static void sendNotification(Context context, Message message) {
+        Intent chattingIntent = new Intent(context, ChattingActivity.class);
+        chattingIntent.putExtra("targetUser", message.getUser().toJsonString());
+//        chattingIntent.putExtra("currentChatRoom", message.get)
+        TaskStackBuilder builder = TaskStackBuilder.create(context);
+        builder.addNextIntentWithParentStack(chattingIntent);
+
+        PendingIntent pendingIntent = builder.getPendingIntent(0,
+                PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT);
 
         String channelId = "my notification channel";
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
@@ -27,7 +39,8 @@ public class NotificationService {
                         .setContentTitle(String.format("%s %s", message.getUser().getFirstName(), message.getUser().getLastName()))
                         .setContentText(message.getContent())
                         .setAutoCancel(true)
-                        .setSound(defaultSoundUri);
+                        .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent);
 
         NotificationManager notificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
